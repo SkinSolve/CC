58e51b23447606f1a556de9e1aecdd3948ec0055
Ilham Eka Prawira <ilhamgenshin911@gmail.com>
2024-06-13 18:56:04 +0700

    add models

--- skinsolve-backend/src/services/loadModel.js
+++ skinsolve-backend/src/services/loadModel.js
@@ -1,7 +1,12 @@
 const tf = require('@tensorflow/tfjs-node');
 
 async function loadModel() {
-    return tf.loadGraphModel(process.env.MODEL_URL);
+    const modelUrls = [
+        "file://skinsolve-backend/models/acne_tfjs/model.json",
+        "file://skinsolve-backend/models/redness_tfjs/model.json",
+        "file://skinsolve-backend/models/skintype_tfjs/model.json"
+      ];
+    return tf.loadLayersModel(process.env.MODEL_URL);
 }
 
 module.exports = loadModel;
